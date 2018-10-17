9767fb215dbbd0860500f348910e79eed8cff6fd
ankitb09 <ankit.b09@gmail.com>
2018-10-17 15:43:04 +0530

    bifurcate

--- bifurcate/bifurcate.js
+++ bifurcate/bifurcate.js
@@ -1,6 +1,8 @@
+
 const bifurcate = (arr, filter) => {
     return arr.reduce((acc, curr, i) => {
-        return acc[filter[i] ? 0 : 1].push(curr),acc;
+        acc[filter[i] ? 0 : 1].push(curr);
+        return acc;
     }, [[], []])
 }
 
