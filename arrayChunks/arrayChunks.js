1a1c8b8670c4a7d059361f61c884a9fa5d74da2e
ankitb09 <ankit.b09@gmail.com>
2018-10-23 11:34:53 +0530

    array to HTML

--- arrayChunks/arrayChunks.js
+++ arrayChunks/arrayChunks.js
@@ -2,9 +2,8 @@ function chunk(arr, size) {
     var len = Math.ceil(arr.length / size);
     var arra = [];
     for (var i = 0; i < len; i++) {
-        var x = arr.slice(i*size,i*size+size);
+        var x = arr.slice(i * size, i * size + size);
         arra.push(x)
-       
     }
     return arra;
 }
