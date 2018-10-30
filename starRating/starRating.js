c3402cca64dae3b37ddbc31f7f97b109a6a33d0d
ankitb09 <ankit.b09@gmail.com>
2018-10-30 15:29:47 +0530

    star rating

--- starRating/starRating.js
+++ starRating/starRating.js
@@ -1,2 +1,25 @@
+// - CreateMainElement
+// - Create whole structre in main Element
+
+// - configurable(5 / 10 star)
+// - if 5 star then - 5 star with complete hover effect
+
+function starRating(elem, ratingType = 5) {
+    let elemUL = document.createElement('ul');
+
+    for (var i = 0; i < 5; i++) {
+        var elemLi = document.createElement('li');
+        elemLi.innerHTML = i + 1;
+        elemUL.appendChild(elemLi);
+        addEvents(elemLi)
+    }
+    elem.appendChild(elemUL)
+}
+
+function addEvents(elem) {
+    elem.addEventListener('click', function () {
+        console.log(this)
+    });
+}
 
 
