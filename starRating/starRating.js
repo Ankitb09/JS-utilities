df457f8a02881ad63479d2b731ae4e45b42fbd2a
ankitb09 <ankit.b09@gmail.com>
2018-10-31 15:27:37 +0530

    star rating

--- starRating/starRating.js
+++ starRating/starRating.js
@@ -1,17 +1,21 @@
-// - CreateMainElement
-// - Create whole structre in main Element
 
-// - configurable(5 / 10 star)
-// - if 5 star then - 5 star with complete hover effect
+var clickedElem;
 
 function starRating(elem) {
     let elemUL = document.createElement('ul');
+    elemUL.classList.add('starRating');
 
-    for (var i = 0; i < 5; i++) {
+    let starSvg = '<svg height="25" width="23" class="star rating" data-rating="4">\
+    <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;"/>\
+  </svg>';
+
+    for (var i = 5; i > 0; i--) {
         var elemLi = document.createElement('li');
-        elemLi.innerHTML = i + 1;
+        elemLi.title = `${i}/5 rating`
+        elemLi.dataset.value = i;
+        elemLi.innerHTML = starSvg;
         elemUL.appendChild(elemLi);
-        addEvents(elemLi)
+        addEvents(elemLi);
     }
     elem.appendChild(elemUL)
 }
@@ -22,16 +26,22 @@ function addEvents(elem) {
         for (var child of this.parentElement.children) {
             child.classList.remove('active')
         }
-
+        clickedElem = this;
         this.classList.add('active')
     });
+
     elem.addEventListener('mouseover', function () {
         for (var child of this.parentElement.children) {
             child.classList.remove('active')
         }
-
+        console.log(this.previousSibling);
         this.classList.add('active')
+    });
+
+    elem.addEventListener('mouseout', function () {
+        for (var child of this.parentElement.children) {
+            child.classList.remove('active')
+        }
+        clickedElem.classList.add('active')
     })
 }
-
-
